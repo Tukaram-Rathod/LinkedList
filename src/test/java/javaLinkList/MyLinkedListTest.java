c63120b83f50671c05bf104dc50b95da9c0e7b7e
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-01 16:28:03 +0530

    Added. inserted 30 node betwwen 56 and 70

--- src/test/java/javaLinkList/MyLinkedListTest.java
+++ src/test/java/javaLinkList/MyLinkedListTest.java
@@ -12,29 +12,46 @@ public class MyLinkedListTest {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add(myFirstNode);
-        myLinkedList.append(mySecondNode);
-        myLinkedList.append(myThirdNode);
-        myLinkedList.printmyNodes();
-        boolean result = myLinkedList.head.equals(myFirstNode) &&
-                         myLinkedList.head.getNext().equals(mySecondNode) &&
-                         myLinkedList.tail.equals(myThirdNode);
-        Assert.assertTrue(result);
-
-    }
-    //test case for appending number 30 between 56 and 70
-    @Test
-    public void given3Numbers_Append_To_LinkedListShouldBeAdded_At_Last() {
-        MyNode<Integer> myThirdNode = new MyNode<>(56);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myFirstNode = new MyNode<>(70);
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.add(myFirstNode);
         myLinkedList.add(mySecondNode);
         myLinkedList.add(myThirdNode);
         myLinkedList.printmyNodes();
         boolean result = myLinkedList.head.equals(myThirdNode) &&
                 myLinkedList.head.getNext().equals(mySecondNode) &&
                 myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
+
+    }
+    //test case for appending number 30 between 56 and 70
+    @Test
+    public void given3Numbers_Append_To_LinkedListShouldBeAdded_At_Last() {
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printmyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+
+    }
+    //inserting node 30 between 56 and 70 nodes
+    @Test
+    public void given3Numbers_Insert_To_LinkedListShouldBeAdded_At_Between() {
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.printmyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
 
     }
